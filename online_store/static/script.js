6467abcea2133f2c24e40704671ff6cbc820cb67
ilnranrbk <67681075+ilnranrbk@users.noreply.github.com>
2024-12-20 20:40:36 +0500

    Add files via upload

---
+++ online_store/static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const shopNowBtn = document.getElementById("shop-now-btn");
+    shopNowBtn.addEventListener("click", () => {
+        window.location.href = "/shop";
+    });
+});
